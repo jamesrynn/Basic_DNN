f664cd395a345bf6de826f4d390578f267caafb0
James <jajrynn@googlemail.com>
2020-03-18 00:01:04 +0000

    added more inputs to Higham example

--- run_higham_examples.m
+++ run_higham_examples.m
@@ -1,8 +1,7 @@
 
 %{
-
-TURN THIS INTO AN INPUT SCRIPT?
-
+FUNCTION DESCRIPTION:
+---------------------------------------------------------------------------
 Run the two examples in the paper 'Deep Learning: An Introduction for
 Applied Mathematicians' (2018) by C F Higham and D J Higham - available on 
 ArXiV: https://arxiv.org/abs/1801.05894
@@ -18,8 +17,9 @@ plot_data = 1;
 track_cost = 1;
 RNG = 5000;
 
-To produce Figures 5/9, run withas above but with
+To produce Figures 5/9, run as above but with
 data = 'paper11';
+---------------------------------------------------------------------------
 
 Written by: James Rynn
 Last edited: 16/03/2020
@@ -27,21 +27,23 @@ Last edited: 16/03/2020
 
 
 
-% Choose example to run.
+%% CHOOSE DATA:
+
 ex_in = input('Please choose data set 1 (Figures 4/8) or 2 (Figures 5/9)? [1/2] (default: 1): \n');
 if(isempty(ex_in) || (ex_in==1))
     data = 'paper10';
 elseif(ex_in==2)
     data = 'paper11';
 end
+load([data '.mat'])
 
 
+%% CHOOSE ACTIVATION FUNCTION:
 
-% Choose activation function.
 fn_in = input('Choose an activation function: [1 (Linear), 2 (Sigmoid), 3 (ReLU), 4 (Leaky ReLU), 5 (ELU)] (default: 2): \n');
 if(fn_in==1)
     fn_type = 'linear';
-elseif((fn_in==2) || isempty(fn_in))
+elseif(isempty(fn_in) || (fn_in==2))
     fn_type = 'sigmoid';
 elseif(fn_in==3)
     fn_type = 'ReLU';
@@ -55,23 +57,24 @@ end
 
 
 
-%% Build vector describing number of nodes in ech hidden layer.
+%% CHOOSE NETWORK STRUCTURE:
 
-% Initialise vector.
+% Initialise vector giving number of nodes in each hidden layer.
 hid_layers = [];
 
 
 % First hidden layer.
-nl1_in = input('Please enter number of nodes in the first hidden layer (default: 3) \n');
-if(isempty(nl1_in))
-    hid_layers = [hid_layers,3];
+nl1_in = input('Please enter number of nodes in the first hidden layer or enter 0 for default hidden layers {layer 1: 3 nodes, layer 2: 4 nodes, layer 3: 5} (default: 0) \n');
+if(isempty(nl1_in) || (nl1_in==0))
+    hid_layers = [3,4,5];
+    layer = 0;
 else
     hid_layers = [hid_layers,nl1_in];
+    layer = 2;
 end
 
 
 % Further hidden layers.
-layer = 2;
 while(layer>0)
     nl_in = input(['Please enter number of nodes in hidden layer ' num2str(layer) ', enter 0 to add no further layers (default: 0) \n']);
     
@@ -79,7 +82,7 @@ while(layer>0)
     if(isempty(nl_in) || (nl_in==0))
         layer = 0;
         
-    % Add layer of appropriate size.
+    % Add layer of requested size.
     else
         hid_layers = [hid_layers, nl_in];
         layer = layer + 1;
@@ -87,19 +90,60 @@ while(layer>0)
 end
 
 
-% % Output architecture.
-% nl = [2,hid_layers,2];
-% print(['Number of nodes in each layer is ' mat2str(nl)])
-    
-    
-% Set experiment parameters.
-M = 1;
-Niter = 1e5;
-eta = 0.05;
+% Output architecture.
+nl = [2,hid_layers,size(y,1)];
+disp(['Number of nodes in each layer is ' mat2str(nl)]); fprintf('\n')
+
+
+
+%% BATCH SIZE:
+
+M_in = input(['Please choose (stochastic gradient descent) batch size (min: 1, max: ' num2str(length(x1)) 'default: 1) \n']);
+if(isempty(M_in))
+    M = 1;
+elseif((M<1) || (M>length(x1)))
+    error(['Batch size M must be an integer between 1 and ' num2str(length(x1))])
+else
+    M = M_in;
+end
+
+
+
+%% NUMBER OF ITERATIONS:
+
+Niter_in = input('Please choose number of gradient descent iterations (default: 100,000) \n');
+if(isempty(Niter_in))
+    Niter = 1e5;
+else
+    Niter = Niter_in;
+end
+
+
+
+%% LEARNING RATE:
+
+eta_in = input('Please choose learning rate (default: 0.05) \n');
+if(isempty(eta_in))
+    eta = 0.05;
+else
+    eta = eta_in;
+end
+
+
+
+%% OTHER PARAMETERS:
+
+% Plot data.
 plot_data = 1;
+
+% Track value of cost functions.
 track_cost = 1;
+
+% Seed random number generator.
 RNG = 5000;
 
 
-% Train network and plot results.
+
+%% TRAIN NETWORK AND MAKE PREDICTIONS:
+
 netbp_cell(data, fn_type, hid_layers, M, Niter, eta, plot_data, track_cost, RNG)
